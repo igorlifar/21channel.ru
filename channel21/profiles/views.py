16e2cdc2fafec0b946d11e6d0412bc19deb5e064
Igor Lifar <igor.lifar@omnihead.ru>
2012-03-30 19:27:59 +0400

    auth app added

---
+++ channel21/profiles/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, redirect
+from django.contrib.auth import authenticate, login, logout
+
+
+def login_panel(request):
+	try:
+		username = request.POST['login']
+		password = request.POST['password']
+		
+		print username
+		print password
+		
+		user = authenticate(username=username, password=password)
+
+		if user is not None and user.is_active:
+			login(request, user)
+			return redirect("/panel/")
+		else:
+			return redirect("/panel/login/?wrongdata=1")
+	except:
+		return redirect("/panel/login/?wrongdata=1")
+		
+def logout_panel(request):
+	logout(request)
+	return redirect("/panel/login/")
