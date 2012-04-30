32be468f276576b4bb4e9b1cf3eccc405fed2b45
Igor Lifar <igor.lifar@omnihead.ru>
2012-04-30 04:06:42 +0400

    scrolls

--- templates/site/content/shows/show.js
+++ templates/site/content/shows/show.js
@@ -1,3 +1,11 @@
+
+$(".episodes-list-scroll").scroll({
+	widthScroll: "15px",
+	heightScroll: "15px",
+	height: "270px",
+	width: "478px"
+});
+
 {% include 'site/content/shows/show/shots.js' %}
 {% include 'site/content/shows/show/articles.js' %}
 {% include 'site/content/shows/show/promo.js' %}
